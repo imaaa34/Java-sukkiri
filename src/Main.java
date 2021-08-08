f1c623bb507277e2a07e8a51f38256a165ade12b
imaaa34 <oct.imd34@gmail.com>
2021-08-08 22:58:15 +0900

    [Finish] chapter9

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,14 @@ public class Main {
 		Zenhan.doTogame();
 		comment.Kouhan.callDemae();
 		comment.Kouhan.showMondokoro();
+		
+		
+		Cleric c = new Cleric("Scott", 40, 8);
+		System.out.println(c.name + "が誕生した！");
+		System.out.println("HP:" + c.hp);
+		System.out.println("MP:" + c.mp);
+		
+		c.selfAid();
+		c.pray(6);
 	}
 }

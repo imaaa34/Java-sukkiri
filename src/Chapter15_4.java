13dfa78b9fda393c3b7f26317048d4fc33996fe2
imaaa34 <oct.imd34@gmail.com>
2021-08-14 12:21:46 +0900

    [Finish] chapter15

--- src/Chapter15_4.java
+++ src/Chapter15_4.java
@@ -2,6 +2,6 @@ import java.io.IOException;
 
 public class Chapter15_4 {
 	public static void main(String[] args) throws IOException {
-		throw new IOException("IOException例外が発生しました。");
+		throw new IOException("IOException例外が発生しました");
 	}
 }

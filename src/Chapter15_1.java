ecc32742cb3c62bdab4e9b44b1824c1ed54a1632
imaaa34 <oct.imd34@gmail.com>
2021-08-14 12:20:44 +0900

    [Finish] chapter15

--- src/Chapter15_1.java
+++ src/Chapter15_1.java
@@ -4,7 +4,7 @@ public class Chapter15_1 {
 			String s = null;
 			System.out.println(s.length());
 		} catch(Exception e) {
-			System.out.println("NullPointerException例外をcatchしました。");
+			System.out.println("NullPointerException例外をcatchしました");
 			System.out.println("--スタックトレース（ここから）--");
 			e.printStackTrace();
 			System.out.println("--スタックトレース（ここまで）--");

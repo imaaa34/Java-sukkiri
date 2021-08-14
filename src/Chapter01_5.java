c9c4d4c3a02b31aa7b717e443665ca39af7304d8
imaaa34 <oct.imd34@gmail.com>
2021-08-14 13:00:52 +0900

    [Fix] error

--- src/Chapter01_5.java
+++ src/Chapter01_5.java
@@ -4,5 +4,11 @@ public class Chapter01_5 {
 		int b = 5;
 		int c = 3 * 5;
 		System.out.println("横幅3縦幅5の長方形の面積は、" + c);
+	
+		boolean result = true;
+		char character = '駆';
+		double pi = 3.14;
+		long piLong = 314159265853979L;
+		String damage = "ミナトの攻撃！敵に15ポイントのダメージを与えた";
 	}
 }
--- src/Chapter02_8.java
+++ src/Chapter02_8.java
@@ -16,6 +16,7 @@ public class Chapter02_8 {
 		fortune++;
 		System.out.println("占いの結果が出ました！");
 		System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
-		 System.out.println("1:大吉 2:中吉　3:吉　4:凶");
+		System.out.println("1:大吉 2:中吉　3:吉　4:凶");
 	}
 }
+                                                                                                                                                                                                                       

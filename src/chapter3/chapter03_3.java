fa715067d88abbe6bc98f1154d371cbf05dbbbc8
imaaa34 <oct.imd34@gmail.com>
2021-08-19 15:55:18 +0900

    [Finish] chapter3

---
+++ src/chapter3/chapter03_3.java
@@ -0,0 +1,16 @@
+package chapter3;
+
+public class chapter03_3 {
+	public static void main(String[] args) {
+		int seibetsu = 0;
+		int age = 25;
+		System.out.println("こんにちは。");
+		if(seibetsu == 0) {
+			System.out.println("私は男です。");
+			System.out.println(age + "歳です。");
+		} else {
+			System.out.println("私は女です。");
+		}
+		System.out.println("よろしくおねがいします。");
+	}
+}

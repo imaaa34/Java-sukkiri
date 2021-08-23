eb5fa84b82f063ff829660dae617fadab58fb4eb
imaaa34 <oct.imd34@gmail.com>
2021-08-23 11:22:29 +0900

    [Finish] chapter6

---
+++ src/chapter6/Main.java
@@ -0,0 +1,12 @@
+package chapter6;
+
+import comment.Zenhan;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment.Kouhan.callDemae();
+		comment.Kouhan.showMondokoro();
+	}
+}
---
+++ src/chapter6/comment/Zenhan.java
@@ -0,0 +1,10 @@
+package chapter6.comment;
+
+public class Zenhan {
+	public static void doWarusa() {
+		System.out.println("きなこでござる。食えませんがの。");
+	}
+	public static void doTogame() {
+		System.out.println("この老ぼれの目はごまかせんぞ。");
+	}
+}

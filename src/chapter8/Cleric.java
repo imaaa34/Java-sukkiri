b290badf4d69a54f985b6c487e042bc9a35152dd
imaaa34 <oct.imd34@gmail.com>
2021-08-27 10:15:35 +0900

    [Finish] chapter8

--- src/chapter8/Cleric.java
+++ src/chapter8/Cleric.java
@@ -2,8 +2,24 @@ package chapter8;
 
 public class Cleric {
 	String name;
-	int HP = 50;
+	int hp = 50;
 	final int MAXHP = 50;
-	int MP = 10;
+	int mp = 10;
 	final int MAXMP = 10;
+	void selfAid() {
+		this.mp -= 5;
+		this.hp = this.MAXHP;
+	}
+	int pray(int p) {
+		int r = new java.util.Random().nextInt(3);
+		int total = p + r;
+		this.mp += total;
+		if(mp > MAXMP) {
+			this.mp = this.MAXMP;
+			total = this.MAXMP - this.mp;
+		}
+//		最小値を求めるメソッド
+//		int actual = Math.min(this.MAXHP - this.mp, total);
+		return total;
+	}
 }
